08b354580bd97f4517cf6f311442c34f6093ff63
Your Name <you@example.com>
2023-03-03 12:51:58 +0530

    fetch url changes

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ app.post("/", async(req,res) =>{
     console.log(req.body);
     let {topic} = req.body;
     let date = new Date();
-    const result = await fetch(`https://newsapi.org/v2/everything?q=${topic}&from=${date.getFullYear()}-${date.getMonth()+1}-${date.getDate()-4}&to=${date.getFullYear()}-${date.getMonth()+1}-${date.getDate()-4 }&sortBy=popularity&apiKey=70c8447e46874793860fbfa10dfbb5f6`);
+    const result = await fetch(`https://newsapi.org/v2/everything?q=${topic}&from=${date.getFullYear()}-${date.getMonth()+1}-${date.getDate()-3}&to=${date.getFullYear()}-${date.getMonth()+1}-${date.getDate() }&sortBy=popularity&apiKey=70c8447e46874793860fbfa10dfbb5f6`);
    
     const response = await result.json();
     const arr = response.articles;
